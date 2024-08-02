0389f2af6d671c0ea64e296e2ee7f482ae50e062
Garo <fayaaguilar@gmail.com>
2024-08-01 23:17:47 -0500

    feat: update conexion.php

--- mi_base_datos.sql
+++ mi_base_datos.sql
@@ -17,25 +17,6 @@ CREATE TABLE usuarios (
     estado ENUM('activo', 'inactivo') DEFAULT 'inactivo'
 );
 
-CREATE DATABASE mi_base_datos;
-
-USE mi_base_datos;
-
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombres VARCHAR(100),
-    apellidos VARCHAR(100),
-    sexo ENUM('masculino', 'femenino'),
-    email VARCHAR(100) UNIQUE,
-    direccion VARCHAR(255),
-    usuario VARCHAR(50) UNIQUE,
-    clave VARCHAR(255),
-    celular VARCHAR(20),
-    plan VARCHAR(50),
-    tipoPlan VARCHAR(50),
-    estado ENUM('activo', 'inactivo') DEFAULT 'inactivo'
-);
-
 CREATE TABLE isotipos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre ENUM('hectomorfo', 'mesomorfo', 'endomorfo') UNIQUE
